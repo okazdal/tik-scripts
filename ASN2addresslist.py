2ff0fe11bb94b6f0d73fcd3606468347f8d6b6c7
okazdal <0aberbyamd>
2017-05-17 11:52:25 +0300

    initial commit

--- ASN2addresslist.py
+++ ASN2addresslist.py
@@ -1,34 +1,28 @@
 __author__ = 'okazdal'
-# whois -h whois.radb.net -- '-i origin AS714' | grep -Eo "([0-9.]+){4}/[0-9]+" | head
+# whois -h whois.radb.net -- '-i origin AS15169' | grep -Eo "([0-9.]+){4}/[0-9]+"
 # GOOGLE  AS15169
 # YOUTUBE AS36040
-import re
 import subprocess
 import shlex
 
-command = shlex.split('whois -h whois.radb.net -- \'-i origin AS714\' | grep -Eo "([0-9.]+){4}/[0-9]+" | head')
-# subprocess
+ASN = 'AS15169'
 
-file = 'youtube.txt'
-address_list_name = 'youtube'
+command = shlex.split('''whois -h whois.radb.net -i origin AS36040''')
+grep_command = shlex.split('grep -Eo "([0-9.]+){4}/[0-9]+"')
 
-fh = open(file, mode='r')
+p1 = subprocess.Popen(command, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(grep_command, stdin=p1.stdout, stdout=subprocess.PIPE)
 
-dest_file = open('google.rsc', mode='w')
-lines = fh.readlines()
-fh.close()
+address_list_name = ASN
+
+dest_file = open(ASN + '.rsc', mode='w')
+lines = p2.stdout.readlines()
+
 
 for line in lines:
-    # line = re.sub(r'route:\W+',
-    #              '',
-    #              line)
-    # line = re.sub(r'\W$', '', line)
-    # line.rstrip('\n')
-    # print line
     chunks = line.split()
-    print chunks[0]
-#     /ip firewall address-list add address=1.1.1.1 list=testlistesi
-    print '/ip firewall address-list add address=' + chunks[0] + ' list=' + address_list_name
+    # print chunks[0]
+    # print '/ip firewall address-list add address=' + chunks[0] + ' list=' + address_list_name
     dest_file.write('/ip firewall address-list add address=' + chunks[0] + ' list=' + address_list_name + '\n')
 
 dest_file.close()
